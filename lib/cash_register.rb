1b4a6afe865ebf711fe8c388a0e2a7a265c0974f
Chase <chaseit12@gmail.com>
2018-02-23 19:18:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
 
+
+class Cash_register
+  
+  attr_accessor :age
+  
+  def initialize(age)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  
+  
+  def add_item(title, price)
+    @total += total
+  end
+  def total
+    @total
+  end
+end
